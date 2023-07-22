01901475dd8a6e5dc3679015386905b5b79038c2
alonr1807 <alonr1807@gmail.com>
2023-07-22 17:43:35 -0500

    Added drop down menu and new cryptocurrency menu

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.png';
+import logo from './cryptosight-logo.png';
 import './App.css';
 import React, { useState } from 'react';
 
@@ -6,45 +6,123 @@ function App() {
 
   const [selectedAlgorithm, setSelectedAlgorithm] = useState('');
 
+  const [selectedCrypto, setSelectedCrypto] = useState('');
+
   const handleAlgorithmChange = (event) => {
-    setSelectedAlgorithm(event.target.value);
+    setSelectedAlgorithm(event.target.textContent);
+    console.log(event.target.textContent);
   }
+  const handleCryptoChange = (event) => {
+    setSelectedCrypto(event.target.textContent);
+    console.log(event.target.textContent);
+  }
+  
 
   return (
     <div className="container">
       <div className="logo-container">
         <img src={logo} className="logo" alt="Logo" />
       </div>
-      <p className = "selecttxt">Select an algorithm:</p>
-    <select value={selectedAlgorithm} onChange={handleAlgorithmChange}>
-      <option value="">-- Choose an algorithm --</option>
-      <option value="hursh">hursh</option>
-      <option value="pika">pika</option>
-      <option value="ethash">Ethash</option>
-      <option value="jew">tony</option>
-    </select>
+      <div class="dropdown">
+        <button class = "selecttxt ">Select an algorithm</button>
+        <div class="dropdown-content">
+          <button onClick={handleAlgorithmChange} class = "algorithm">first</button>
+          <button onClick={handleAlgorithmChange} class = "algorithm">second</button>
+          <button onClick={handleAlgorithmChange} class = "algorithm">third</button>
+          <button onClick={handleAlgorithmChange} class = "algorithm" >fourth</button>
+          <button onClick={handleAlgorithmChange} class = "algorithm" >fifth</button>
+        </div>
+      </div>
+      
+
+
+
+    <div class="dropdown">
+      <button class="cryptotxt">Crypto Currency</button>
+      <div class="dropdown-content">
+        <button onClick={handleCryptoChange} class = "btc">Bitcoin</button>
+        <button onClick={handleCryptoChange} class = "eth">Ethereum</button>
+        <button onClick={handleCryptoChange} class = "tet">Tether</button>
+        <button onClick={handleCryptoChange} class = "xrp">XRP</button>
+        <button onClick={handleCryptoChange} class = "bnb" >BNB</button>
+      </div>
+    </div>
 
     {/* Display insights based on the selected algorithm */}
     {selectedAlgorithm && (
       <div className="insights">
-        {selectedAlgorithm === "ethash" ? (
+        {selectedAlgorithm === "Bitcoin" ? (
           <>
             <h2>Insights for {selectedAlgorithm}</h2>
-            <p>Correct choice, you will get rich</p>
+            <p>Correct choice, you will get rich EDIT</p>
           </>
-        ) : selectedAlgorithm === "jew" ? (
+        ) : selectedAlgorithm === "Ethereum" ? (
           <>
             <h2>Insights for {selectedAlgorithm}</h2>
-            <p>wrong choice</p>
+            <p>wrong choice EDIT</p>
+          </>
+        ) : selectedAlgorithm === "Tether" ? (
+          <>
+            <h2>Insights for {selectedAlgorithm}</h2>
+            <p>wrong choiceEDIT</p>
+          </>
+        ) : selectedAlgorithm === "XRP" ? (
+          <>
+            <h2>Insights for {selectedAlgorithm}</h2>
+            <p>wrong choiceEDIT</p>
+          </>
+        ) : selectedAlgorithm === "BNB" ? (
+          <>
+            <h2>Insights for {selectedAlgorithm}</h2>
+            <p>wrong choiceEDIT</p>
           </>
         ) : (
           <>
             <h2>Insights for {selectedAlgorithm}</h2>
-            <p>Wrong</p>
+            <p>WrongEDIT</p>
           </>
         )}
       </div>
     )}
+
+    {/* Display insights based on the selected crypto */}
+    {selectedCrypto && (
+      <div className="insights">
+        {selectedCrypto === "first" ? (
+          <>
+            <h2>Insights for {selectedCrypto}</h2>
+            <p>Correct choice, you will get rich EDIT</p>
+          </>
+        ) : selectedCrypto === "second" ? (
+          <>
+            <h2>Insights for {selectedCrypto}</h2>
+            <p>wrong choice EDIT</p>
+          </>
+        ) : selectedCrypto === "third" ? (
+          <>
+            <h2>Insights for {selectedCrypto}</h2>
+            <p>wrong choiceEDIT</p>
+          </>
+        ) : selectedCrypto === "fourth" ? (
+          <>
+            <h2>Insights for {selectedCrypto}</h2>
+            <p>wrong choiceEDIT</p>
+          </>
+        ) : selectedCrypto === "fifth" ? (
+          <>
+            <h2>Insights for {selectedCrypto}</h2>
+            <p>wrong choiceEDIT</p>
+          </>
+        ) : (
+          <>
+            <h2>Insights for {selectedCrypto}</h2>
+            <p>WrongEDIT</p>
+          </>
+        )}
+      </div>
+    )}
+
+
   </div>
   );
 }
