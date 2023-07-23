0514de3c313054974a2f0102a42b4598a388a691
hursh-shah <123204365+hursh-shah@users.noreply.github.com>
2023-07-22 22:24:23 -0700

    Fixed the website ngl

--- src/App.js
+++ src/App.js
@@ -1,129 +1,127 @@
 import logo from './cryptosight-logo.png';
 import './App.css';
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { Chart, LineController, LinearScale, CategoryScale, PointElement, LineElement } from 'chart.js';
+Chart.register(LineController, LinearScale, CategoryScale, PointElement, LineElement);
 
-function App() {
-
-  const [selectedAlgorithm, setSelectedAlgorithm] = useState('');
-
-  const [selectedCrypto, setSelectedCrypto] = useState('');
+function AlgorithmPage({ setSelectedAlgorithm }) {
+  const [dropdownVisible, setDropdownVisible] = useState(false);
+  const [selected, setSelected] = useState('Select an algorithm');
 
   const handleAlgorithmChange = (event) => {
     setSelectedAlgorithm(event.target.textContent);
-    console.log(event.target.textContent);
+    setSelected(`${event.target.textContent} selected`);
+    setDropdownVisible(false);
   }
-  const handleCryptoChange = (event) => {
-    setSelectedCrypto(event.target.textContent);
-    console.log(event.target.textContent);
-  }
-  
 
   return (
     <div className="container">
       <div className="logo-container">
         <img src={logo} className="logo" alt="Logo" />
       </div>
-      <div class="dropdown">
-        <button class = "algotxt">Select an algorithm</button>
-        <div class="dropdown-content">
-          <button onClick={handleAlgorithmChange} class = "algorithm">first</button>
-          <button onClick={handleAlgorithmChange} class = "algorithm">second</button>
-          <button onClick={handleAlgorithmChange} class = "algorithm">third</button>
-          <button onClick={handleAlgorithmChange} class = "algorithm" >fourth</button>
-          <button onClick={handleAlgorithmChange} class = "algorithm" >fifth</button>
+      <div className="dropdown" onMouseEnter={() => setDropdownVisible(true)} onMouseLeave={() => setDropdownVisible(false)}>
+        <button className="algotxt">{selected}</button>
+        <div className="dropdown-content" style={{display: dropdownVisible ? 'block' : 'none'}}>
+          <button onClick={handleAlgorithmChange} className="algorithm">first</button>
+          <button onClick={handleAlgorithmChange} className="algorithm">second</button>
+          <button onClick={handleAlgorithmChange} className="algorithm">third</button>
+          <button onClick={handleAlgorithmChange} className="algorithm">fourth</button>
+          <button onClick={handleAlgorithmChange} className="algorithm">fifth</button>
         </div>
       </div>
-      
+      <Link to="/crypto" className="next-link" style={{marginTop: dropdownVisible ? '200px' : '50px'}}>Next</Link>
+    </div>
+  );
+}
 
+function CryptoPage({ setSelectedCrypto }) {
+  const [dropdownVisible, setDropdownVisible] = useState(false);
+  const [selected, setSelected] = useState('Select a Crypto');
 
+  const handleCryptoChange = (event) => {
+    setSelectedCrypto(event.target.textContent);
+    setSelected(`${event.target.textContent} selected`);
+    setDropdownVisible(false);
+  }
 
-    <div class="dropdown">
-      <button class="cryptotxt">Crypto Currency</button>
-      <div class="dropdown-content">
-        <button onClick={handleCryptoChange} class = "btc">Bitcoin</button>
-        <button onClick={handleCryptoChange} class = "eth">Ethereum</button>
-        <button onClick={handleCryptoChange} class = "tet">Tether</button>
-        <button onClick={handleCryptoChange} class = "xrp">XRP</button>
-        <button onClick={handleCryptoChange} class = "bnb" >BNB</button>
+  return (
+    <div className="container">
+      <div className="logo-container">
+        <img src={logo} className="logo" alt="Logo" />
+      </div>
+      <div className="dropdown" onMouseEnter={() => setDropdownVisible(true)} onMouseLeave={() => setDropdownVisible(false)}>
+        <button className="cryptotxt">{selected}</button>
+        <div className="dropdown-content" style={{display: dropdownVisible ? 'block' : 'none'}}>
+          <button onClick={handleCryptoChange} className="btc">Bitcoin</button>
+          <button onClick={handleCryptoChange} className="eth">Ethereum</button>
+          <button onClick={handleCryptoChange} className="tet">Tether</button>
+          <button onClick={handleCryptoChange} className="xrp">XRP</button>
+          <button onClick={handleCryptoChange} className="bnb">BNB</button>
+        </div>
+      </div>
+      <Link to="/" className="back-link" style={{marginTop: dropdownVisible ? '200px' : '70px'}}>Back</Link>
+      <Link to="/stats" className="next-link" style={{marginTop: dropdownVisible ? '200px' : '70px'}}>Next</Link>
+    </div>
+  );
+}
+
+function LineChart({ data, labels }) {
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    const chart = new Chart(canvasRef.current, {
+      type: 'line',
+      data: {
+        labels: labels,
+        datasets: [{
+          data: data,
+          backgroundColor: 'rgba(0, 123, 255, 0.1)',
+          borderColor: 'rgba(0, 123, 255, 1)',
+          borderWidth: 1
+        }]
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+      }
+    });
+
+    return () => chart.destroy();
+  }, [data, labels]);
+
+  return <canvas ref={canvasRef}></canvas>;
+}
+
+function StatisticsPage({ selectedAlgorithm, selectedCrypto }) {
+  const data = [12, 19, 3, 5, 2, 3];  // dummy data
+  const labels = ['January', 'February', 'March', 'April', 'May', 'June'];  // dummy labels
+
+  return (
+    <div className="container">
+      <h1>Trading Statistics</h1>
+      <p>Algorithm: {selectedAlgorithm}</p>
+      <p>Crypto: {selectedCrypto}</p>
+      <div style={{ height: '400px', width: '100%' }}>
+        <LineChart data={data} labels={labels} />
       </div>
     </div>
-
-    {/* Display insights based on the selected algorithm */}
-    {selectedAlgorithm && (
-      <div className="insights">
-        {selectedAlgorithm === "first" ? (
-          <>
-            <h2>Insights for {selectedAlgorithm}</h2>
-            <p>Correct choice, you will get rich EDIT</p>
-          </>
-        ) : selectedAlgorithm === "second" ? (
-          <>
-            <h2>Insights for {selectedAlgorithm}</h2>
-            <p>wrong choice EDIT</p>
-          </>
-        ) : selectedAlgorithm === "third" ? (
-          <>
-            <h2>Insights for {selectedAlgorithm}</h2>
-            <p>wrong choiceEDIT</p>
-          </>
-        ) : selectedAlgorithm === "fourth" ? (
-          <>
-            <h2>Insights for {selectedAlgorithm}</h2>
-            <p>wrong choiceEDIT</p>
-          </>
-        ) : selectedAlgorithm === "fifth" ? (
-          <>
-            <h2>Insights for {selectedAlgorithm}</h2>
-            <p>wrong choiceEDIT</p>
-          </>
-        ) : (
-          <>
-            <h2>Insights for {selectedAlgorithm}</h2>
-            <p>WrongEDIT</p>
-          </>
-        )}
-      </div>
-    )}
-
-    {/* Display insights based on the selected crypto */}
-    {selectedCrypto && (
-      <div className="insights">
-        {selectedCrypto === "Bitcoin" ? (
-          <>
-            <h2>Insights for {selectedCrypto}</h2>
-            <p>Correct choice, you will get rich EDIT</p>
-          </>
-        ) : selectedCrypto === "Ethereum" ? (
-          <>
-            <h2>Insights for {selectedCrypto}</h2>
-            <p>wrong choice EDIT</p>
-          </>
-        ) : selectedCrypto === "Tether" ? (
-          <>
-            <h2>Insights for {selectedCrypto}</h2>
-            <p>wrong choiceEDIT</p>
-          </>
-        ) : selectedCrypto === "XRP" ? (
-          <>
-            <h2>Insights for {selectedCrypto}</h2>
-            <p>wrong choiceEDIT</p>
-          </>
-        ) : selectedCrypto === "BNB" ? (
-          <>
-            <h2>Insights for {selectedCrypto}</h2>
-            <p>wrong choiceEDIT</p>
-          </>
-        ) : (
-          <>
-            <h2>Insights for {selectedCrypto}</h2>
-            <p>WrongEDIT</p>
-          </>
-        )}
-      </div>
-    )}
+  );
+}
 
 
-  </div>
+function App() {
+  const [selectedAlgorithm, setSelectedAlgorithm] = useState('');
+  const [selectedCrypto, setSelectedCrypto] = useState('');
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/crypto" element={<CryptoPage setSelectedCrypto={setSelectedCrypto} />} />
+        <Route path="/stats" element={<StatisticsPage selectedAlgorithm={selectedAlgorithm} selectedCrypto={selectedCrypto} />} />
+        <Route path="/" element={<AlgorithmPage setSelectedAlgorithm={setSelectedAlgorithm} />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/testscraper.js
+++ src/testscraper.js
@@ -11,16 +11,25 @@ async function scrapeProduct() {
 
     await page.goto('https://www.quantconnect.com/')
     await page.click('#qc-navbar-collapse > ul > li.top.dropdown > a');
-    await page.waitForTimeout(1000);  
-    await page.type('#headerSignInEmail', 'throweway15@gmail.com');
-    await page.waitForTimeout(1000);
-    await page.type('#headerSignInPassword', 'QzpMkef!_Ne#Bz8'); 
+    await page.waitForTimeout(2000);  
+    await page.click('#social-media-links > a:nth-child(1) > button');
+    await page.waitForTimeout(2000);
+    await page.type('#identifierId', 'throweway15@gmail.com');
+    await page.waitForTimeout(2000);
+
+    await page.click('#identifierNext > div > button > div.VfPpkd-RLmnJb');
+    await page.waitForTimeout(2000);
+    // // #identifierId
+    // #password > div.aCsJod.oJeWuf > div > div.Xb9hP > input
+    await page.type('#password > div.aCsJod.oJeWuf > div > div.Xb9hP > input', 'hurshV1507'); 
     await page.waitForTimeout(3000);
-    await page.click('#loginForm > div:nth-child(9) > button');
-    await page.waitForTimeout(2500);  
+    // await page.click('#loginForm > div:nth-child(9) > button');
+    await page.click('#passwordNext > div > button > div.VfPpkd-RLmnJb');
+    await page.waitForTimeout(3000);  
 
     await page.goto('https://www.quantconnect.com/project/15382513/');
-    await page.waitForTimeout(10000);  
+
+    await page.waitForTimeout(10000);
 
 
 
@@ -37,14 +46,18 @@ async function scrapeProduct() {
         await page.keyboard.press('Backspace');
     }
     await page.keyboard.press("Enter");
-    await page.waitForTimeout(500);
+    await page.waitForTimeout(2000);
     let str = 'self.symbol = self.AddCrypto("BTCUSD", Resolution.Hour).Symbol';
     for(let i = 0; i < str.length; i++) {
         await page.keyboard.press(str.charAt(i));
     }
-    await page.waitForTimeout(5000);
-    await page.click("#workbench\.parts\.editor > div.content > div.grid-view-container > div > div > div > div.monaco-scrollable-element.mac > div.split-view-container > div > div > div.title.tabs.show-file-icons > div.tabs-and-actions-container > div.editor-actions > div > div > ul > li:nth-child(2) > a");
-    await page.keyboard.press('Control', "F5")
+    await page.waitForTimeout(4000);
+    // await page.click('#workbench\.parts\.editor > div.content > div.grid-view-container > div > div > div > div.monaco-scrollable-element.mac > div.split-view-container > div > div > div.title.tabs.show-file-icons > div.tabs-and-actions-container > div.monaco-scrollable-element.mac > div.tabs-container');
+    // await page.click('#workbench\.parts\.editor > div.content > div.grid-view-container > div > div > div > div.monaco-scrollable-element.mac > div.split-view-container > div > div > div.title.tabs.show-file-icons > div.tabs-and-actions-container > div.editor-actions > div > div > ul > li:nth-child(2) > a');
 
+    // await page.click("#workbench\.parts\.editor > div.content > div.grid-view-container > div > div > div > div.monaco-scrollable-element.mac > div.split-view-container > div > div > div.title.tabs.show-file-icons > div.tabs-and-actions-container > div.editor-actions > div > div > ul > li:nth-child(2) > a");
+    await page.keyboard.press("Control", "s") 
+    await page.waitForTimeout(1000);
+    await page.keyboard.press('Control', "F5")
 };
 scrapeProduct();
